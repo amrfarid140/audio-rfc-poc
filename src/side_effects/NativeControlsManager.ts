a779e2ab5b20062612345a4f4d13f81f4e5b0d03
Amr Yousef <amr.farid140@gmail.com>
2022-09-14 21:46:54 +0100

    Fix native notifications

--- src/side_effects/NativeControlsManager.ts
+++ src/side_effects/NativeControlsManager.ts
@@ -14,7 +14,6 @@ type RNMusicControlOptions = {
   duration: number; // Seconds
   speed: PlaybackSpeed | 0;
   title: string;
-  state: any;
 };
 
 const musicControlPlaybackStatusMap = {
@@ -33,14 +32,6 @@ export class NativeControlsManager
     MusicControl.enableControl(Command.play, true);
     MusicControl.enableControl(Command.pause, true);
     MusicControl.enableControl(Command.togglePlayPause, true);
-    MusicControl.on(Command.play, this.myAudioManager.togglePlayback);
-    MusicControl.on(Command.pause, this.myAudioManager.togglePlayback);
-    MusicControl.on(
-      Command.togglePlayPause,
-      this.myAudioManager.togglePlayback,
-    );
-    MusicControl.on(Command.nextTrack, this.myQueueManager.next);
-    MusicControl.on(Command.previousTrack, this.myQueueManager.previous);
   }
 
   onStateUpdated(
@@ -54,13 +45,32 @@ export class NativeControlsManager
       duration: 22,
       speed,
       title: 'React is a UI Framework',
-      state: musicControlPlaybackStatusMap[status],
     };
     MusicControl.setNowPlaying(options);
+    MusicControl.updatePlayback({state: musicControlPlaybackStatusMap[status]});
+    MusicControl.on(Command.play, () => {
+      this.myAudioManager.togglePlayback().catch(console.error);
+    });
+    MusicControl.on(Command.pause, () => {
+      this.myAudioManager.togglePlayback().catch(console.error);
+    });
+    MusicControl.on(Command.togglePlayPause, () => {
+      this.myAudioManager.togglePlayback().catch(console.error);
+    });
+    MusicControl.on(Command.nextTrack, this.myQueueManager.next);
+    MusicControl.on(Command.previousTrack, this.myQueueManager.previous);
+    MusicControl.enableControl(
+      Command.nextTrack,
+      this.myQueueManager.currentState().length !== 0,
+    );
+    MusicControl.enableControl(
+      Command.previousTrack,
+      this.myQueueManager.currentState().length !== 0,
+    );
     // Update native controls with status and speed
   }
   onQueueUpdated(queue: AudioQueue): void {
-    MusicControl.enableControl(Command.nextTrack, queue.length != 0);
-    MusicControl.enableControl(Command.previousTrack, queue.length != 0);
+    MusicControl.enableControl(Command.nextTrack, queue.length !== 0);
+    MusicControl.enableControl(Command.previousTrack, queue.length !== 0);
   }
 }
